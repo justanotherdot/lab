ef406df43f82c35aa74646f9e0cf1c4e2aad3f4b
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-09-08 21:01:14 +1000

    Broken impl. for utf8 parser
    The shifts for composing the various bytes are always signed, which is
    clearly wrong; we need unsigned shifts to keep 0's in place and allow
    the ORing to do it's thing properly, i.e.
    
    `wint_t wide_char = byte1 | byte2 << 8;`
    
    will give:
    
    `1111 1111 1111 1111 1111 1111 1101 1011`
    
    where `byte1` is `1101 1011` in binary.

--- C/utf8_parser.h
+++ C/utf8_parser.h
@@ -45,9 +45,13 @@ UTF8ParseResult utf8_parse_result_err(char* str);
 
 // random.
 
+void print_utf8_str(char* str, size_t len);
+
 void analyse_bytes_of_str(char* str, size_t len);
 
 void maybe_print_locale_present();
+
+void print_int(int c);
 
 void print_byte(char c);
 
