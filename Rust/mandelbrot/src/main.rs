9ae6aac52d443f49f0fd32b14505f1a05188dd49
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-09-25 20:30:26 +1000

    Map pixels to complex numbers

--- Rust/mandelbrot/src/main.rs
+++ Rust/mandelbrot/src/main.rs
@@ -17,6 +17,7 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
 }
 
 #[allow(dead_code)]
+/// Parse a pair of floating point numbers separated by a comma as a complex number.
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
         Some((re, im)) => Some(Complex { re, im }),
@@ -37,6 +38,29 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
+/// Given the row and column of a pixel in the output image, return the corresponding point to the
+/// complex plain.
+///
+/// 'bounds' is a pair giving the width and height of the image in pixels. 'pixel' is a (column,
+/// row) pair indicating a particular pixel in that image. The 'upper_left' and 'lower_left'
+/// parameters are points on the complex plane designating the area our image covers.
+#[allow(dead_code)]
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                           upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+        // Why subtraction here? pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -50,4 +74,13 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100),
+                              (25, 75),
+                              Complex { re: -1.0, im: 1.0 },
+                              Complex { re: 1.0, im: -1.0 }),
+                Complex { re: -0.5, im: -0.5 });
 }
