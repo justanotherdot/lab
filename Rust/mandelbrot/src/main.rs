0979d328bb9fffb26f29559663605d33b61d2ec9
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-09-25 20:46:26 +1000

    Encode PNGs via write_image

--- Rust/mandelbrot/src/main.rs
+++ Rust/mandelbrot/src/main.rs
@@ -1,7 +1,26 @@
 extern crate num;
-use num::Complex;
+extern crate image;
 
+use num::Complex;
 use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the file named `filename`.
+#[allow(dead_code)]
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+    -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels,
+                   bounds.0 as u32,
+                   bounds.1 as u32,
+                   ColorType::Gray(8))?;
+    Ok(())
+}
 
 #[allow(dead_code)]
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
@@ -41,8 +60,8 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
 /// Given the row and column of a pixel in the output image, return the corresponding point to the
 /// complex plain.
 ///
-/// 'bounds' is a pair giving the width and height of the image in pixels. 'pixel' is a (column,
-/// row) pair indicating a particular pixel in that image. The 'upper_left' and 'lower_left'
+/// `bounds` is a pair giving the width and height of the image in pixels. `pixel` is a (column,
+/// row) pair indicating a particular pixel in that image. The `upper_left` and `lower_left`
 /// parameters are points on the complex plane designating the area our image covers.
 #[allow(dead_code)]
 fn pixel_to_point(bounds: (usize, usize),
@@ -58,6 +77,34 @@ fn pixel_to_point(bounds: (usize, usize),
         im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
         // Why subtraction here? pixel.1 increases as we go down,
         // but the imaginary component increases as we go up.
+    }
+}
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`, which holds one
+/// grayscale pixel per byte. The `upper_left` and `lower_right` arguments specify points on the
+/// complex plane corresponding to the upper- left and lower-right corners of the pixel buffer.
+#[allow(dead_code)]
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          upper_left: Complex<f64>,
+          lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds,
+                                       (column, row),
+                                       upper_left,
+                                       lower_right);
+            pixels[row * bounds.0 + column] =
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                };
+        }
     }
 }
 
