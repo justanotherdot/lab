57ced316564000b58f4cabf131bb76563214c50f
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-09-27 20:10:37 +1000

    Sequential main functionality

--- Rust/mandelbrot/src/main.rs
+++ Rust/mandelbrot/src/main.rs
@@ -1,11 +1,12 @@
 extern crate num;
 extern crate image;
 
-use num::Complex;
-use std::str::FromStr;
 use image::ColorType;
 use image::png::PNGEncoder;
+use num::Complex;
 use std::fs::File;
+use std::io::Write;
+use std::str::FromStr;
 
 // Write the buffer `pixels`, whose dimensions are given by `bounds`, to the file named `filename`.
 #[allow(dead_code)]
@@ -109,7 +110,30 @@ fn render(pixels: &mut [u8],
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+            .unwrap();
+        writeln!(std::io::stderr(), "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0])
+            .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
 
 #[test]
